381bb76ec9437801e0457a7949ae47da7b569a5d
kbfoot <kelsey.klora@gmail.com>
2016-10-17 14:24:58 -0700

    Allows URL img input

--- public/js/intelligentapp.js
+++ public/js/intelligentapp.js
@@ -1,13 +1,16 @@
-$('#btn').click(function () {
+$('#imgURL').change(function () {
+
+ var imgURL = document.getElementById("imgURL").value;
     $.ajax({
+     
         url: "https://api.projectoxford.ai/vision/v1.0/tag",        
         beforeSend: function (xhrObj) {
             xhrObj.setRequestHeader("Content-Type", "application/json");
             xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "1509752abed946fe8d21cc1d998286d5");
             },
             type: "POST",
-            data: '{"url": "https://oxfordportal.blob.core.windows.net/emotion/recognition1.jpg"}'
-           
+            data: "{'url':'" + imgURL+ "'}",
+      
             })
             
             .done(function (data) {
@@ -18,16 +21,20 @@ $('#btn').click(function () {
 
                 for(var i = 0, l = msgLen; i < l; i++) {
                     var msg = msgTag[i];
-                   // var div = document.createElement('div');
+              
                     $("#response").append("#" + msg.name + " ");
-                 //   document.body.appendChild($("#response"));
+      
                 }
-               // $("#response").text(JSON.stringify(data));
+          
             })
 
             .fail(function (error) {
                 $("#response").text(error);
-            })
-        });      
+                alert(error.getAllResponseHeaders());
 
+            })
+        });    
+
+
+        
 
