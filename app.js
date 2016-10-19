92355cf61860904cab1712608083fe366a3bbb1c
kbfoot <kelsey.klora@gmail.com>
2016-10-19 14:06:52 -0700

    app.js fix

--- app.js
+++ app.js
@@ -4,23 +4,8 @@ var app = express();
 var server = require('http').createServer(app); 
 var io = require('socket.io').listen(server);
 var port = process.env.PORT || 3000;
-var oxford = require('project-oxford');
-var client = new oxford.Client(process.env.myoxfordkey);
+
 
 server.listen(port);
 
 app.use(express.static(__dirname + '/public'));
-
-io.on('connection', function(socket) { 
-    socket.on('origimgurl', function(origimgurl) {
-        console.log('face url: ' + origimgurl);
-        client.face.detect({
-            url: origimgurl,
-            analyzesFaceLandmarks: true,
-            analyzesHeadPose: true
-        }).then(function(response) {
-            socket.emit('face', response);
-            console.log(response);
-            });
-        });
-    });
